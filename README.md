d4c9aad51ed7cde45a4b1ba0b405af2c83a37c52
euygor <ygor.oliveiragb@gmail.com>
2022-08-06 06:54:58 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Sistema de Hambúrguers
+
+Projeto em Vue3 + Json-server.
+
+### Instalação
+- `npm install`
+
+### Para rodar
+- `npm run dev`
---
+++ src/components/Banner.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="main-banner" :style="image">
+        <h1>Make Your Burger</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Banner',
+    data: function () {
+        return {
+            image: { backgroundImage: 'url(/assets/images/burger.jpg)' },
+        };
+    },
+};
+</script>
+
+<style scoped>
+#main-banner {
+    background-position: 0 -250px;
+    background-size: cover;
+    height: 500px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+}
+#main-banner h1 {
+    color: #fff;
+    text-align: center;
+    font-size: 60px;
+    background-color: #222;
+    padding: 20px 40px;
+}
+</style>
